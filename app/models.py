760412b335d6100c029189a6a056c07c92ed66b7
Enoch Jason J <enochjason06@gmail.com>
2024-12-19 12:11:09 +0530

    Created API

--- app/models.py
+++ app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class TaskModel(models.Model):
+    taskName = models.CharField(max_length=150)
+
+
+    def __str__(self):
+        return self.taskName
--- app/views.py
+++ app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.generics import CreateAPIView, ListAPIView
 
+from .models import TaskModel
+from .serializers import TaskSerializer
 # Create your views here.
+
+
+
+class TaskCreateView(generics.CreateAPIView):
+    queryset = TaskModel.objects.all()
+    serializer_class = TaskSerializer
+
+
+class TaskListView(generics.ListAPIView):
+    queryset = TaskModel.objects.all()
+    serializer_class = TaskSerializer
